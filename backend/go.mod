eb2c16e63623ac65cef0e9183c5740f2a8ccb987
Nikodem Bienia <nikodem.bienia@envelio.de>
2024-05-19 16:55:19 +0200

    UPDATE: go dependencies and tests

--- backend/go.mod
+++ backend/go.mod
@@ -8,7 +8,12 @@ require (
 )
 
 require (
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 	golang.org/x/text v0.15.0 // indirect
--- backend/readTournamentFromDb/crud_test.go
+++ backend/readTournamentFromDb/crud_test.go
@@ -1,7 +1,9 @@
 package readTournamentFromDb_test
 
 import (
+	// "encoding/json"
 	"fmt"
+	// "os"
 	"testing"
 
 	"github.com/derdem/iamtoolazytotip/postgres_connection"
@@ -17,3 +19,17 @@ func TestReadTournament(t *testing.T) {
 	tournament := readTournamentFromDb.ReadTournament(2)
 	fmt.Println(tournament)
 }
+
+// func _TestPrepareDataDump(t *testing.T) {
+// 	readTournamentFromDb.GetConnection = postgres_connection.GetConnectionForTest
+
+// 	tournament := readTournamentFromDb.GetTournament(2)
+// 	jsonString, err := json.Marshal(tournament)
+// 	if err != nil {
+// 		t.Errorf("Error marshalling tournament: %v", err)
+// 	}
+// 	fmt.Println(string(jsonString))
+
+// 	os.WriteFile("../dumps/tournament2.json", jsonString, 0644)
+
+// }
